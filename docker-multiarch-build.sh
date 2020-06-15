eb891e80eeb8db2645d8e225d04a0736bd0112f4
Chris Cowley <chris.cowley@fr.clara.net>
2020-06-15 22:20:59 +0200

    stuff

--- docker-multiarch-build.sh
+++ docker-multiarch-build.sh
@@ -3,8 +3,8 @@
 TAG=$(git describe --tags $(git rev-list --tags --max-count=1))
 SHA=$(git rev-parse --short HEAD)
 
-for docker_arch in amd64 arm32v6 arm64v8; do
-  docker build -t docker.io/chriscowley/paperless:${TAG}-${SHA}-${docker_arch} --build-arg ARCH=${docker_arch}.
+for docker_arch in amd64 arm32v7 arm64v8; do
+  docker build -t docker.io/chriscowley/paperless:${TAG}-${SHA}-${docker_arch} --build-arg ARCH=${docker_arch} .
   docker tag docker.io/chriscowley/paperless:${TAG}-${SHA}-${docker_arch} docker.io/chriscowley/paperless:latest-${docker_arch}
 done
-sed "{tag}/${TAG}/g" manifest.yml.in manifest.yml
+sed "s/{tag}/${TAG}-${SHA}/g" manifest.yml.in > manifest.yml
